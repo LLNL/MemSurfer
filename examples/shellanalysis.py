2182de40171e95c7ae86687995627beb16e8c247
Harsh Bhatia <bhatia4@llnl.gov>
2022-05-09 14:46:13 -0700

    handled separate fatslim file for thickness

--- examples/shellanalysis.py
+++ examples/shellanalysis.py
@@ -17,6 +17,7 @@ import sys
 import glob
 import numpy as np
 import argparse
+import random
 
 import logging
 from datetime import datetime
@@ -35,11 +36,23 @@ periodic = True
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-def read_fatslim_to_df(filename):
-    LOGGER.info(f"Reading ({filename})")
-    df = pd.read_csv(filename)
-    df = df.rename(columns={"X coords": "pos_x", "Y coords": "pos_y",
-                            "Z coords": "pos_z", "Area per lipid": "apl(fatslim)"})
+def read_fatslim_to_df(aplfile, thckfile):
+
+    LOGGER.info(f"Reading ({aplfile}) and ({thckfile})")
+
+    df1 = pd.read_csv(aplfile)
+    df2 = pd.read_csv(thckfile, usecols = ['resid','Thickness'])
+
+    df1.set_index(['resid'], inplace=True)
+    df2.set_index(['resid'], inplace=True)
+
+    df = df1.join(df2).reset_index()
+
+    df = df.rename(columns={"X coords": "pos_x",
+                            "Y coords": "pos_y",
+                            "Z coords": "pos_z",
+                            "Area per lipid": "apl(fatslim)",
+                            "Thickness": "thickness(fatslim)"})
     df['leaflet'] = df['leaflet'].apply(lambda x: x.split(' ')[0])
     LOGGER.info(f"Read {df.shape} data")
     return df
@@ -74,18 +87,23 @@ def fetch_helices(u):
 
 
 # ------------------------------------------------------------------------------
-def process_frame(trajfile, fslmfile, outfile):
+def need_processing(trajfile, aplfile, thckfile, outfile=None):
 
-    if not os.path.isfile(trajfile):
-        LOGGER.warning(f'File ({trajfile}) not found!')
-        return False
+    for f in [trajfile, aplfile, thckfile]:
+        if not os.path.isfile(f):
+            #LOGGER.warning(f'File ({f}) not found!')
+            return False
 
-    if not os.path.isfile(fslmfile):
-        LOGGER.warning(f'File ({fslmfile}) not found!')
-        return False
+    if outfile is not None:
+        if os.path.isfile(outfile):
+            #LOGGER.warning(f'File ({outfile}) already present!')
+            return False
 
-    if os.path.isfile(outfile):
-        LOGGER.info(f'File ({outfile}) already exists!')
+    return True
+
+def process_frame(trajfile, aplfile, thckfile, outfile):
+
+    if not need_processing(trajfile, aplfile, thckfile, outfile):
         return False
 
     # --------------------------------------------------------------------------
@@ -111,15 +129,19 @@ def process_frame(trajfile, fslmfile, outfile):
         assert nhelices == prot_coords[nm].shape[0]
         assert nhelices == prot_resnames[nm].shape[0]
 
+    helix_idxs = np.arange(nhelices)
+    helix_resids = -1 * np.ones(nhelices, dtype=int)
+
     helix_nans = np.empty(nhelices)
     helix_nans.fill(np.nan)
-    helix_resids = -1 * np.ones(nhelices, dtype=int)
-    helix_idxs = np.arange(nhelices)
+    helix_ord_param = np.empty((nhelices,2))
+    helix_ord_param.fill(np.nan)
+    #ord_param_helices = -1*np.ones((nhelices, 2))
 
     LOGGER.info(f'Extracted {nhelices} helices')
 
     # --------------------------------------------------------------------------
-    df = read_fatslim_to_df(fslmfile)
+    df = read_fatslim_to_df(aplfile, thckfile)
 
     # --------------------------------------------------------------------------
     # This is where we use MemSurfer
@@ -139,6 +161,7 @@ def process_frame(trajfile, fslmfile, outfile):
                             hg_df['pos_y'].to_numpy(),
                             hg_df['pos_z'].to_numpy()))[0]
         hg_apl = hg_df['apl(fatslim)'].to_numpy()
+        hg_thck = hg_df['thickness(fatslim)'].to_numpy()
 
         # find residue names for all these residues using mdanalysis
         hg = u.select_atoms('resid '+' '.join([str(h) for h in hg_resids]))
@@ -148,7 +171,7 @@ def process_frame(trajfile, fslmfile, outfile):
         nchol = sum(hg_resnames == 'CHOL')
 
         LOGGER.info(f'leaflet {nm}: '
-                    f'nresidues = {nresidues}, nCHOL = {nchol} ',
+                    f'nresidues = {nresidues}, nCHOL = {nchol} '
                     f'unique values = {np.unique(hg_resnames)}')
 
         # ----------------------------------------------------------------------
@@ -208,13 +231,12 @@ def process_frame(trajfile, fslmfile, outfile):
         # order parameters
         z = m.memb_smooth.attributes['pnormal']
         ord_param_chol = np.ones((nchol, 2))
-        ord_param_helices = -1*np.ones((nhelices, 2))
         ord_param_lips = utils.get_order_param(tail_2a.atoms.positions,
                                                tail_3a.atoms.positions,
                                                tail_2b.atoms.positions,
                                                tail_3b.atoms.positions,
                                                z[nhelices+nchol:])
-        order_params = np.concatenate((ord_param_helices, ord_param_chol, ord_param_lips))
+        order_params = np.concatenate((helix_ord_param, ord_param_chol, ord_param_lips))
 
         # grab densities and labels from the membrane!
         params = {key: val for key, val in m.attributes.items()}
@@ -226,9 +248,10 @@ def process_frame(trajfile, fslmfile, outfile):
         # for comparison and reference, we want to look at fatslim apl
         if 1:
             params['apl(fatslim)'] = np.concatenate((helix_nans, hg_apl))
+            params['thickness(fatslim)'] = np.concatenate((helix_nans, hg_thck))
             params['resids'] = np.concatenate((helix_resids, hg_resids))
 
-        if 1:
+        if 0:
             m.memb_smooth.write_vtp(os.path.join(path_out, f'{frame}-{nm}.vtp'), params)
             #m.write_all(os.path.join(path_out, f'all-{frame}-{nm}'), params)
 
@@ -247,7 +270,8 @@ def process_frame(trajfile, fslmfile, outfile):
                    'pos_x', 'pos_y', 'pos_z',
                    'pnormal_x', 'pnormal_y', 'pnormal_z',
                    'shell', 'parea', 'mean_curv', 'gaus_curv',
-                   'apl(fatslim)','order_param_a','order_param_b'
+                   'order_param_a','order_param_b',
+                   'apl(fatslim)', 'thickness(fatslim)'
                    ]]
 
     LOGGER.info(f'Writing {mdf.shape} dataframe to ({outfile})')
@@ -275,28 +299,32 @@ if __name__ == '__main__':
     parser.add_argument('--filegro', dest='ingro',
                         help='Path to input trajectory file (*.gro)')
 
-    parser.add_argument('--filefat', dest='infat',
-                        help='Path to input fatslim file (*.csv)')
+    parser.add_argument('--fileapl', dest='inapl',
+                        help='Path to input APL file (*.csv)')
+
+    parser.add_argument('--filethck', dest='inthck',
+                        help='Path to input thickness file (*.csv)')
 
     args = parser.parse_args()
 
-    mode_frame = (args.ingro is not None) and (args.infat is not None)
+    path_in = args.inpath
+    path_out = args.outpath
+
+    mode_frame = (args.ingro is not None) and (args.inapl is not None) and (args.inthck is not None)
     mode_traj = args.inpath is not None
+
 
     if mode_traj == mode_frame:
         print ('Need either a frame (gro + fatslim) or an input directory!\n')
         parser.print_help()
         exit(1)
 
-    # --------------------------------------------------------------------------
-    #path_out = os.path.join(args.outpath, 'shellanalysis', 'memsurfer')
-    path_out = args.outpath
     os.makedirs(path_out, exist_ok=True)
 
     # --------------------------------------------------------------------------
     print(f'using memsurfer from ({os.path.dirname(memsurfer.__file__)})')
 
-    if 1:
+    if 0:
         memsurfer.utils.create_logger(1, 1,0, '','')
     else:
         ts = datetime.now().strftime('%Y%m%d-%H%M%S')
@@ -306,56 +334,88 @@ if __name__ == '__main__':
     # find files to read
     # --------------------------------------------------------------------------
     gro2frame = lambda f: int(os.path.basename(f)[5:-4])
-    fat2frame = lambda f: int(os.path.basename(f)[:-4].split('_')[-1])
+    apl2frame = lambda f: int(os.path.basename(f)[:-4].split('_')[-1])
+    thck2frame = lambda f: int(os.path.basename(f)[:-4].split('_')[0].split('.')[-1])
+    has_all = lambda f: len(f[0])>0 and len(f[1])>0 and len(f[2])>0
 
     files = {}
 
     if mode_frame:
-        print (args.ingro)
         t1 = gro2frame(args.ingro)
-        print (args.infat)
-        t2 = fat2frame(args.infat)
-        assert t1 == t2, f'Input files have mismatch in frame number: {t1} != {t2}'
+        t2 = apl2frame(args.inapl)
+        t3 = thck2frame(args.inthck)
 
-        files[t1] = [args.ingro, args.infat]
+        assert t1 == t2, f'Input files have mismatch in frame number: {t1} != {t2}'
+        files[t1] = [args.ingro, args.inapl, args.inthck]
 
     else:
-        path_in = args.inpath
+
         path_gro = os.path.join(path_in, 'frames')
-        path_fslm = os.path.join(path_in, 'output')
+        path_fslm = os.path.join(path_in, 'APL')
+        path_thck = os.path.join(path_in, 'thickness')
 
-        files_gro = sorted(glob.glob(os.path.join(path_gro, 'frame*.gro')))
-        files_fslm = sorted(glob.glob(os.path.join(path_fslm, 'fatslim-output.frame_*.csv')))
+        files_gro = glob.glob(os.path.join(path_gro, 'frame*.gro'))
+        files_fslm = glob.glob(os.path.join(path_fslm, 'fatslim-output.frame_*.csv'))
+        files_thck = glob.glob(os.path.join(path_thck, 'fatslim-thickness.*_frame_00000.csv'))
 
-        print (f'found {len(files_gro)} gro files in ({path_gro})')
-        print (f'found {len(files_fslm)} fatslim files in ({path_fslm})')
+        random.shuffle(files_gro)
+
+        LOGGER.info (f'found {len(files_gro)} gro files in ({path_gro})')
+        LOGGER.info (f'found {len(files_fslm)} fatslim files in ({path_fslm})')
+        LOGGER.info (f'found {len(files_thck)} fatslim files in ({path_thck})')
 
         for f in files_gro:
             t = gro2frame(f)
-            files[t] = [f, '']
+            files[t] = [f, '', '']
 
         for f in files_fslm:
-            t = fat2frame(f)
+            t = apl2frame(f)
             if t in files:
                 files[t][1] = f
             else:
-                files[t] = ['', f]
+                files[t] = ['', f, '']
+
+        for f in files_thck:
+            t = thck2frame(f)
+            if t in files:
+                files[t][2] = f
+            else:
+                files[t] = ['', '', f]
+
+        files = {t: f for t,f in files.items() if has_all(f)}
+
+    LOGGER.info(f'Processing {len(files)} files')
+    if mode_frame:
+        LOGGER.info(files)
+
+
+    #_outfile = lambda tf: os.path.join(path_out, f'{os.path.basename(tf)[:-4]}-memsurfer.csv')
+    #files = {_:f for _,f in files.items() if need_processing(f[0],f[1],f[2], _outfile(f[0]))}
+    #print (f'found {len(files)} files to process')
+    #exit(1)
+
+
 
     # --------------------------------------------------------------------------
     # Compute membranes for all frames
     # --------------------------------------------------------------------------
     for frame, files in files.items():
 
-        trajfile = files[0]
-        fslmfile = files[1]
-        trajname = os.path.basename(files[0])[:-4]
+        trajfile, aplfile, thckfile = files[0], files[1], files[2]
+
+        trajname = os.path.basename(trajfile)[:-4]
         outfile = os.path.join(path_out, f'{trajname}-memsurfer.csv')
+        #outfile = _outfile(trajfile)
+
+        if os.path.isfile(outfile):
+            LOGGER.info(f'Skipping frame {frame} because ({outfile}) already exists!')
+            continue
 
         try:
-            process_frame(trajfile, fslmfile, outfile)
+            process_frame(trajfile, aplfile, thckfile, outfile)
 
         except Exception as e:
-            LOGGER.error(f'Failed for ({trajname}): {e}')
+            LOGGER.error(f'Failed for ({frame}): {e}')
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
